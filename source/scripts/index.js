f0917436462695a88be1fa1218b9d84220f12af8
Пакри <pakri@yandex.ru>
2024-03-09 15:34:45 +0300

    Реализация закрытия мобильного меню через нажатие Esc + правка записи события обработчика с on на addEventListener

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -11,8 +11,17 @@ nav.classList.remove('nav--nojs');
 address.classList.remove('address--nojs');
 
 //Открытие/закрытие бургер-меню.
-navToggle.onclick = function () {
+navToggle.addEventListener('click', () => {
   navToggle.classList.toggle('nav__toggle--open-burger');
   navToggle.classList.toggle('nav__toggle--close-burger');
   navWrapper.classList.toggle('nav__wrapper--phone-menu-open');
-};
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    navWrapper.classList.remove('nav__wrapper--phone-menu-open');
+    navToggle.classList.remove('nav__toggle--close-burger');
+    navToggle.classList.add('nav__toggle--open-burger');
+  }
+});
